d2908400284b070d0bc7156e94ccaa6416751883
Mark Krasnokuysky <Mark120320041234@mail.ru>
2024-04-04 02:25:58 +0500

    Рефактор страницы LoginPage.

--- stankin-services-portal-web-ui/stankin-service-portal-frontend/src/pages/LoginPage.tsx
+++ stankin-services-portal-web-ui/stankin-service-portal-frontend/src/pages/LoginPage.tsx
@@ -1,9 +1,8 @@
-import {useState} from "react";
 import axios from 'axios';
+import AuthInput from "../components/AuthInput.tsx";
+import AuthButton from "../components/AuthButton.tsx";
 
 const LoginPage = () => {
-    const [login, setLogin] = useState({login : ''});
-    const [password, setPassword] = useState({password: ''});
 
     async function checkUser() {
         try {
@@ -18,37 +17,23 @@ const LoginPage = () => {
         }
     }
 
-    const handleInputLogin = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
-
-        setLogin((prevInputs) => ({
-            ...prevInputs,
-            [name]: value,
-        }));
-    };
-
-    const handleInputPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
-        //console.log(e.target);
-        setPassword((prevInputs) => ({
-            ...prevInputs,
-            [name]: value,
-        }));
-    };
-
     const handleClickBtn = async (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
         await checkUser();
     }
-    console.log(login);
-    console.log(password);
     return (
         <form>
-            <div style={{display: 'flex', flexDirection: 'column', gap: '6px'}}>
-                <input placeholder='Введите логин' name='login' onChange={e => handleInputLogin(e)}></input>
-                <input placeholder='Введите пароль' name='password' onChange={e => handleInputPassword(e)}></input>
-                <button type='submit' onClick={e => handleClickBtn(e)}>Войти</button>
-            </div>
+            <AuthInput
+                    type="text"
+                    placeholder="Введите логин"
+                    name='login'
+            />
+            <AuthInput
+                    type="password"
+                    placeholder="Введите логин"
+                    name='password'
+            />
+            <AuthButton type="submit" onClick={e => handleClickBtn(e)}>Войти</AuthButton>
         </form>
     );
 };
