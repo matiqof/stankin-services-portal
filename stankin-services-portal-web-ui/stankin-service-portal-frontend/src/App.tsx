6f792f0a64e59fbb69e3f9b47097ccb3b6504d23
Mark Krasnokuysky <Mark120320041234@mail.ru>
2024-04-09 22:19:59 +0500

    Рефактор роутинга.

--- stankin-services-portal-web-ui/stankin-service-portal-frontend/src/App.tsx
+++ stankin-services-portal-web-ui/stankin-service-portal-frontend/src/App.tsx
@@ -1,15 +1,9 @@
 import "./App.css";
-import {Routes, Route} from "react-router-dom";
-import LoginPage from "./pages/LoginPage.tsx";
+import {Outlet} from "react-router-dom";
 function App() {
   return (
     <>
-        {/*<Link to="/auth">*/}
-        {/*    Auth*/}
-        {/*</Link>*/}
-        <Routes>
-            <Route path="/auth" element={<LoginPage/>}/>
-        </Routes>
+        <Outlet></Outlet>
     </>
   );
 }
